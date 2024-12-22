13d8ed38cc85dd92806895adfd9bde489ba7a1ef
Tanner Bennett <tannerbennett@me.com>
2024-12-22 03:30:07 -0600

    Update swift-atomics

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-atomics.git", from: "0.0.1")
+    .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0")
   ],
   targets: [
     .target(
